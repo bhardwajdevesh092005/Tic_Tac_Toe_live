87e1b86906ea9e8e78429ddf7f1475839cadf141
Devesh Bhardwaj <b23cs1014@iitj.ac.in>
2024-07-27 16:26:35 +0530

    Done with the frontend part

--- client/Tic_Tac_Toe_Client/src/components/ModalResult.jsx
+++ client/Tic_Tac_Toe_Client/src/components/ModalResult.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { animate, motion, stagger } from 'framer-motion'
-
+import { animate, motion, stagger, transform } from 'framer-motion'
+import { Link } from 'react-router-dom'
+import { duration } from '@mui/material'
 const variants = {
     hidden: {
         opacity: 0, scaleX: 0, scaleY: 0
@@ -24,22 +25,34 @@ const children = {
         opacity: 1, scale: 1, rotate: 0
     }
 }
+const children1 = {
+    hidden: {
+        opacity: 1,scale:0,rotate:-180,
+    },
+    visible: {
+        opacity: 1,scale:1, rotate:0,
+        transition:{
+            duration:0.5
+        }
+    }
+}
 
 function ModalResult({ won }) {
-    const result = ()=>{
-        if(won === -1){
+    const result = () => {
+        if (won === -1) {
             return "GAME DRAW";
-        }else if(won === 1){
-            return "YOU WON";
-        }else{
-            return "YOU LOOSE";
+        } else if (won === 1) {
+            return "PLAYER 2 WON";
+        } else {
+            return "PLAYER 1 WON";
         }
     }
     return (
         <div className='fixed inset-0 bg-[#0008] flex justify-center'>
-            <motion.div variants={variants} animate={"visible"} initial={"hidden"} className={`w-1/2 h-1/2 rounded-xl bg-yellow-100 self-center ${(won === 1)? "text-green-500" : (won === 2?"text-red-600":"text-black")} text-8xl text-center flex flex-col justify-center`}>
-                <motion.img variants={children} src="/Logo.png" className='w-1/4 h-1/3 self-center rounded-xl mb-16' alt="" />  
+            <motion.div variants={variants} animate={"visible"} initial={"hidden"} className={`w-1/2 h-1/2 rounded-xl bg-yellow-100 self-center ${(won === 1) ? "text-green-500" : (won === 2 ? "text-red-600" : "text-black")} text-8xl text-center flex flex-col justify-center`}>
+                <motion.img variants={children} src="/Logo.png" className='w-1/4 h-1/3 self-center rounded-xl mb-16' alt="" />
                 <motion.div variants={children} className='self-center'>{result()}</motion.div>
+                <motion.img variants={children1} className='self-center text-white w-[100px] h-[100px]' src="/Reload.png" onClick={() => window.location.reload()}></motion.img>
             </motion.div>
         </div>
     )
--- client/Tic_Tac_Toe_Client/src/components/Pages/Play.jsx
+++ client/Tic_Tac_Toe_Client/src/components/Pages/Play.jsx
@@ -3,21 +3,23 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion'
 import ModalResult from '../ModalResult';
-
+import CircularProgress from '@mui/material/CircularProgress';
 function Play() {
     const navigator = useNavigate();
     const state = useSelector((state) => state.status);
     const [cur_user, setCurUser] = useState(1);
-    const [time, setTime] = useState(10);
+    const [time, setTime] = useState(20);
     const [border, setBorder] = useState(`2px solid rgb(${255 * time / 10},${255 * (10 - time) / 10},0)`);
     const [gameState, setGameState] = useState([-1, -1, -1, -1, -1, -1, -1, -1, -1])
     const [bg_resource, setBgResource] = useState(new Array(gameState.length).fill(""));
     const [won, setWon] = useState(-1);
     const [decide, setDecide] = useState(false);
     const win_states = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    const [text_color, setTextColor] = useState(`rgb(${String((255 * (20 - time)) / 20)},${String((255 * time) / 20)},0)`)
     const handleGame = (index) => {
         setGameState((prev) => (prev.map((elem, index1) => (index === index1 ? ((cur_user === 1) ? 1 : 2) : elem))))
         setCurUser((prev) => (prev === 1 ? 2 : 1))
+        setTime(20);
         console.log(gameState);
     }
     useEffect(() => {
@@ -57,32 +59,34 @@ function Play() {
             navigator("/");
         }
     }, [state]);
-    // useEffect(() => {
-    //     setInterval(() => {
-    //         if (time <= 0) {
-    //             setTime(10);
-    //         }
-    //         setTime(prev => prev - 1)
+    useEffect(() => {
+        setInterval(() => {
+            if (time <= 0) {
+                setTime(20);
+            }
+            setTime(prev => prev - 1)
 
-    //     }, 1000);
-    // }, [setTime])
+        }, 1000);
+    }, [setTime])
     useEffect(() => {
         if (time <= 0) {
-            setTime(10);
+            setTime(20);
             if (cur_user == 1) {
                 setCurUser(2);
             } else {
                 setCurUser(1);
             }
         }
-        setBorder(`2px solid rgb(${String((255 * (10 - time)) / 10)},${String((255 * time) / 10)},0)`);
+        setBorder(`2px solid rgb(${String((255 * (20 - time)) / 20)},${String((255 * time) / 20)},0)`);
+        setTextColor(`rgb(${String((255 * (20 - time)) / 20)},${String((255 * time) / 20)},0)`);
     }, [time]);
     return (
         <div className='w-screen h-screen text-white flex flex-col justify-center align-middle bg-black'>
             {decide && <ModalResult won={won} />}
             <img src="/Logo.png" className='w-[200px] h-1/7 self-center rounded-xl mt-2' alt="" />
-            <div className='w-1/2 flex self-center justify-between mt-4'>
+            <div className={`w-1/2 ${!decide ? "flex" : "hidden"}  self-center justify-between mt-4`}>
                 <div style={{ border: `${cur_user === 1 ? border : ""}` }} className={`border-2 w-fit p-2 text-3xl rounded-xl`}>Player 1</div>
+                <div style={{ color: `${text_color}` }} className={`align-middle self-center text-5xl `}><CircularProgress className='w-[100px]' value={100 * (time) / 20} variant='determinate' /></div>
                 <div style={{ border: `${cur_user === 2 ? border : ""}` }} className={`border-2 w-fit p-2 text-3xl rounded-xl`}>Player 2</div>
             </div>
             <div className='w-full h-full flex justify-center'>
